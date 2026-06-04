7200e05cae8fd7e4207d97a17bb77e93bdb9916d
kanishka <kanishkayogamoorthy@gmail.com>
2026-06-04 15:39:33 +0530

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,92 @@
+// var a=10;
+// let b=20;
+// const c=30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="kanishka";
+// var undefine;
+// var Null=null;
+// var boolen= true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof Null);
+// console.log(typeof boolen);
+
+// var bigInt = 123n;
+// var symbol =Symbol('li');
+
+// console.log(typeof bigInt)
+// console.log(typeof symbol)
+
+// var arr =[10,20,30,40];
+// console.log(typeof arr,arr);
+// var obj ={
+//     name:"kanishka",
+//     dept:["AIDS","CT"]
+
+// }
+// console.log(typeof obj,obj)
+
+// var a=10;
+// var b="deep";
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a%b)
+// console.log(a/b)
+
+// true  true true true
+// false true false true
+// true  false false true
+// false false false false
+
+// var a=true;
+// var b =false;
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a)
+
+//relational operator (>,<,<=,>=,==,!=)
+
+// a=10;
+// b="10";
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a!=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a===b);
+ 
+//assingment operator (=,+=,-=,%=,*=,**=)
+
+// a=10;
+// b=20;
+
+// a=a+b;
+// a+=b;
+
+// console.log(a);
+// a-=b;
+// console.log(a);
+
+// unary operator (++,--,)
+
+
+console.log(a++);
+console.log(++a);
+
+var a=10;
+
+console.log(a--);
+console.log(--a);
+
